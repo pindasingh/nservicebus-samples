a8578037b14b24bf9e1a95e880663de782ce5c74
Pinda Singh <pinda.stark@gmail.com>
2024-10-26 09:57:40 +0100

    Add CommandHandlerWithLearningTransport

---
+++ CommandHandlerWithLearningTransport/Endpoint/CreateNewUserCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.Commands;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using System.Threading.Tasks;
+
+namespace Endpoint;
+public class CreateNewUserCommandHandler : IHandleMessages<CreateNewUserCommand>
+{
+    private readonly ILogger _logger;
+
+    public CreateNewUserCommandHandler(ILogger<CreateNewUserCommandHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(CreateNewUserCommand message, IMessageHandlerContext context)
+    {
+        _logger.LogInformation($"Creating user {message.Name} with email {message.EmailAddress}");
+        return Task.CompletedTask;
+    }
+}
---
+++ CommandHandlerWithLearningTransport/Endpoint/Worker.cs
@@ -0,0 +1,25 @@
+﻿using Contracts.Commands;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Endpoint;
+public class Worker : BackgroundService
+{
+    private readonly IMessageSession messageSession;
+
+    public Worker(IMessageSession messageSession)
+    {
+        this.messageSession = messageSession;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await messageSession.SendLocal(new CreateNewUserCommand("bob@test.com", "Bob"), stoppingToken);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
